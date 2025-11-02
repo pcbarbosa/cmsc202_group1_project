31fa92cf8cd1948de299f7013ab898c2c26d6a46
rin.samantha <ongsamanthaerin@gmail.com>
2025-11-02 16:46:24 +0800

    -added caesar cipher on main

--- mastermind.py
+++ mastermind.py
@@ -15,11 +15,27 @@ color_set = {
 def checker_file():
     print("Check File")
 
-def encrypt_password():
-    print("Encrypt Password")
+# Shift each letter in the text by the value of CAESAR_SHIFT while non-letters remain unchanged
+def caesar_shift(text, shift): 
+    result = ""
+    for character in text:
+        if "A" <= character <= "Z":
+            result += chr((ord(character) - ord("A") + shift) % 26 + ord("A"))
+        elif "a" <= character <= "z":
+            result += chr((ord(character) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            result += character
 
-def decrypt_password():
-    print("Decrypt Password")
+    return result
+
+
+def encrypt_password(password): 
+    return caesar_shift(password, CAESAR_SHIFT)
+
+
+def decrypt_password(password): 
+    return caesar_shift(password, -CAESAR_SHIFT)
+
 
 def is_username_exist ():
     print("Check Username")
